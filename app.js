6044242806ca0ecd1f6c5c6de6ac9ae018af9b96
Youngki Ahn <avej.com@gmail.com>
2018-10-29 19:33:21 +0900

    [MOD] Add a favicon

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-var createError = require('http-errors');
+'use strict';
+
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var createError = require('http-errors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,6 +16,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,21 +25,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.post('/update_user_uid',
+	function(req, res)
+	{
+		const crypto = require("crypto");
+		const id = crypto.randomBytes(12).toString("hex");
+		res.json(
+			{
+				"id" : id
+			}
+		);
+	}
+);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use(
+	function(err, req, res, next) {
+		// set locals, only providing error in development
+		res.locals.message = err.message;
+		res.locals.error = req.app.get('env') === 'development' ? err : {};
+		
+		// render the error page
+		res.status(err.status || 500);
+		res.render('error');
+	}
+);
 
 module.exports = app;
